da5a914415fd52e4d3812f7285b8f45a60b4677d
lyubitza <78766801+lyubitza@users.noreply.github.com>
2021-05-31 22:05:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+   alert("La suma es:");
+
+   var raulM = 4;
+   var pedroM = 2;
+   var pedroF = 4;
+   var albertoP = 8;
+
+   res1 = raulM + pedroM;
+   res2 = pedroF/2;
+   res3 = raulM + pedroM + pedroF + albertoP;
+
+
+   document.write(res1);
+  
+   document.write(res2);
+
+   document.write(res3);
